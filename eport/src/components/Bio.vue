a9b62d9600575dd16b6f3457f5a61487f0d252d9
jamiedishy <jamiedishy@gmail.com>
2021-01-04 18:40:22 -0500

    Update - bio and edu

--- eport/src/components/Bio.vue
+++ eport/src/components/Bio.vue
@@ -1,36 +1,44 @@
 <template>
   <div>
-      <div class="container">
-        <div class="row">
-            <div class="col">
-                <div id="info">
-                    <p>Bio</p>
-                    <p>Aaron Dishy is an archivist and information policy researcher based in Toronto, Canada. He holds a Bachelor of Arts in communications and art history from McGill University (2016), as well as a Master of Information in critical information policy studies and archives and records management from the University of Toronto (2018). Aaron’s work centers on new media policy, archives and records management, digital preservation, and the implementation of information and communication technologies.</p>
-                </div>            
-            </div>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div id="info">
+            <p>Bio</p>
+            <p>
+              Aaron Dishy is a digital archivist, and student at Osgoode Hall
+              Law School (2023) (expected), based in Toronto, Canada. He holds a
+              Bachelor of Arts in art history and religious studies from McGill
+              University (2016), as well as a Master of Information in critical
+              information policy studies and archives and records management
+              from the University of Toronto (2018). Aaron’s work centers new
+              media policy, archives management, intellectual property law, and
+              the implementation of information and communication technologies.
+            </p>
+          </div>
         </div>
-      </div>    
-   
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'aaron-bio'
-}
+  name: "aaron-bio",
+};
 </script>
 
 <style>
 #info {
-    padding: 2% 0%;
+  padding: 2% 0%;
 }
 
 #info a {
-    color: black;
+  color: black;
 }
 
 #info a:hover {
-    color: grey;
-    transition: 0.3s;
+  color: grey;
+  transition: 0.3s;
 }
 </style>
--- eport/src/components/Header.vue
+++ eport/src/components/Header.vue
@@ -1,49 +1,48 @@
 <template>
   <div>
     <head>
-        <link rel="icon" href="Mail.png" type="image/gif" sizes="16x16">
-        <title>Aaron Dishy</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="author" content="Jamie Dishy">
+      <link rel="icon" href="Mail.png" type="image/gif" sizes="16x16" />
+      <title>Aaron Dishy</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="author" content="Jamie Dishy" />
     </head>
 
     <div class="container" id="nav">
-        <div class="row">
-            <div class="col-sm">
-                <p >Aaron Dishy</p>
-            </div>
-            <div class="col-sm">
-                <a href="mailto:hello@aarondishy.net">hello@aarondishy.net</a>
-            </div>
-            <div class="col-sm">
-                <p id="nav-space">(514)-266-6428</p>
-            </div>
+      <div class="row">
+        <div class="col-sm">
+          <p>Aaron Dishy</p>
         </div>
+        <div class="col-sm">
+          <a href="mailto:hello@aarondishy.net">hello@aarondishy.net</a>
+        </div>
+        <div class="col-sm">
+          <p id="nav-space">(514)-266-6428</p>
+        </div>
+      </div>
     </div>
-
   </div>
 </template>
 
 <script>
 export default {
-  name: 'aaron-header'
-}
+  name: "aaron-header",
+};
 </script>
 
 <style scoped>
 a {
-    color: black;
+  color: black;
 }
 a:hover {
-    color: grey;
-    transition: 0.3s;
+  color: grey;
+  transition: 0.3s;
 }
 #nav {
-    padding-top: 60px;
+  padding-top: 60px;
 }
 @media only screen and (max-width: 760px) {
-    #nav {
-        display: none;
-    }
+  #nav {
+    display: none;
+  }
 }
 </style>
